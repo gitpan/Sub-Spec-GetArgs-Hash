882523558a81fdf9c100780faef22c3470cccb2a
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-20 08:59:19 +0000

    Import of SHARYANTO/Sub-Spec-GetArgs-Hash-0.02 from CPAN.
    gitpan-cpan-distribution: Sub-Spec-GetArgs-Hash
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Sub-Spec-GetArgs-Hash-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sub/Spec/GetArgs/Hash.pm
+++ lib/Sub/Spec/GetArgs/Hash.pm
@@ -4,7 +4,7 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 our %SPEC;
 
@@ -20,7 +20,7 @@ Sub::Spec::GetArgs::Hash - Get subroutine arguments %args from hash (%args) :)
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -28,9 +28,15 @@ version 0.01
 
 =head1 DESCRIPTION
 
+B<NOTICE>: This module and the L<Sub::Spec> standard is deprecated as of Jan
+2012. L<Rinci> is the new specification to replace Sub::Spec, it is about 95%
+compatible with Sub::Spec, but corrects a few issues and is more generic.
+C<Perinci::*> is the Perl implementation for Rinci and many of its modules can
+handle existing Sub::Spec sub specs.
+
 This is an empty distribution. Created just as a reminder that you don't need to
 do anything if you already have a hash. Sub::Spec-following subroutines already
-accepts hash as arguments by default. (You'd think that this reminder is
+accept hash as arguments by default. (You'd think that this reminder is
 unnecessary, but I've known to think stranger things and forget even simpler
 facts :))
 
@@ -69,7 +75,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2012 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
